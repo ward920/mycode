b3a627ffae27a73328d880bd12ef97a6a5468bb0
ward920 <ward920@users.noreply.github.com>
2022-03-08 17:51:52 +0000

    copying local files with python

---
+++ strings/stringmethod03.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+    kentstr = "Kent loves it when Dan Wanker calls him Jent"
+    kentstrlower = kentstr.lower() 
+    newjentstr = kentstr.split(" ")
+    addjent = " ".join(kentstr)
+    print(newjentstr)
+    print(kentstrlower)
+    print(addjent)
+
+main()
