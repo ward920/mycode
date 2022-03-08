8c400466cade3e56a8656804881623017d404bee
ward920 <ward920@users.noreply.github.com>
2022-03-08 20:49:57 +0000

    writing custom code

---
+++ cust_if_script.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   if, elif, else - A simple program using conditionals to make a decision."""
+
+
+message = 'The movie you just watched '
+
+# wrap connection in a float() to accept decimals as numbers
+rating = float(input("What is your movie rating? On a scale of 1-10, 1 worst, 10 best: "))
+
+# if input value was higher or equal to 10
+if rating >= 10:
+    message = message + 'is up to Kent perfection standards.'
+elif rating >= 5:
+    message = message + 'is up to Dan perfection standards.'
+elif rating >= 2:
+    message = message + 'is up to Wes perfection standards.'
+else:
+    message = message + 'is up to Jeff perfection standards.'
+print(message)
