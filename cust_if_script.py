56bcbce34707f6602fd0dc3eb8d958c96841799f
ward920 <ward920@users.noreply.github.com>
2022-03-08 21:36:56 +0000

    writing custom code

--- cust_if_script.py
+++ cust_if_script.py
@@ -9,12 +9,17 @@ message = 'The movie you just watched '
 rating = float(input("What is your movie rating? On a scale of 1-10, 1 worst, 10 best: "))
 
 # if input value was higher or equal to 10
-if rating >= 10:
+if rating == 42:
+    print('Where you are going, you wont need a guide. Now go hitch a ride!')
+elif rating >= 10:
     message = message + 'is up to Kent perfection standards.'
+    print(message)
 elif rating >= 5:
     message = message + 'is up to Dan perfection standards.'
+    print(message)
 elif rating >= 2:
     message = message + 'is up to Wes perfection standards.'
+    print(message)
 else:
     message = message + 'is up to Jeff perfection standards.'
-print(message)
+    print(message)
